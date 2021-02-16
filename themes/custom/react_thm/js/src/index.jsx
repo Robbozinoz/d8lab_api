84e390f6837237ccb19ee5fb8195c3654757a67f
jamjar_hub <web@thehub.net.au>
2021-02-16 16:47:07 +0800

    Dynamic API call to project api in React theme

--- themes/custom/react_thm/js/src/index.jsx
+++ themes/custom/react_thm/js/src/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// # Example 1: Simple "Hello, World" code
-ReactDOM.render(
-  <h1>Hello there - world!</h1>,
-  document.getElementById('react-app')
+/* Import Components */
+import DrupalProjectStats from './components/DrupalProjectStats';
+
+const Main = () => (
+  <DrupalProjectStats projectName="drupal" />
 );
+
+ReactDOM.render(<Main/>, document.getElementById('react-app'));
